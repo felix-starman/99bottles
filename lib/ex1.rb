304d81a243b52e8cbaa18a239de93c163977d8c8
Sandi Metz <skmetz@gmail.com>
2014-07-21 14:45:48 -0400

    commit not shameless variants

---
+++ lib/ex1.rb
@@ -0,0 +1,36 @@
+class Bottles
+  def verse(number)
+    if number == 2
+      answer_bottles = "#{number - 1} bottle"
+      number_bottles = "#{number} bottles"
+      one = "one"
+      "#{number_bottles} of beer on the wall, #{number_bottles} of beer.\n" +
+      "Take #{one} down and pass it around, #{answer_bottles} of beer on the wall.\n"
+    elsif number == 1
+      answer_bottles = "no more bottles"
+      number_bottles = "1 bottle"
+      one = "it"
+      "#{number_bottles} of beer on the wall, #{number_bottles} of beer.\nTake #{one} down and pass it around, #{answer_bottles} of beer on the wall.\n"
+    elsif number == 0
+      nmcaps = "No more bottles"
+      nmlow = "no more bottles"
+      answer_bottles = "99 bottles"
+      "#{nmcaps} of beer on the wall, #{nmlow} of beer.\nGo to the store and buy some more, #{answer_bottles} of beer on the wall.\n"
+    else
+      answer_bottles = "#{number - 1} bottles"
+      number_bottles = "#{number} bottles"
+      one = "one"
+      "#{number_bottles} of beer on the wall, #{number_bottles} of beer.\nTake #{one} down and pass it around, #{answer_bottles} of beer on the wall.\n"
+    end
+  end
+
+  def verses(start, finish)
+    start.downto(finish).collect do |number|
+      verse(number)
+    end.join("\n")
+  end
+
+  def song
+    verses(99, 0)
+  end
+end
---
+++ lib/ex2.rb
@@ -0,0 +1,41 @@
+class Bottles
+
+  def verse(number)
+    case number
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer.\n" +
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    else
+      "#{bottles(number)} of beer on the wall, #{bottles(number)} of beer.\n" +
+      "Take #{bottle(number)} down and pass it around, #{bottles(number - 1)} of beer on the wall.\n"
+    end
+  end
+
+  def verses(first, last)
+    first.downto(last).inject('') do |song, verse|
+      song << verse(verse) + "\n"
+    end.chomp
+  end
+
+  def song
+    verses(99, 0)
+  end
+
+private
+
+  def bottles(number)
+    case number
+    when 0
+      'no more bottles'
+    when 1
+      '1 bottle'
+    else
+      "#{number} bottles"
+    end
+  end
+
+  def bottle(number)
+    number == 1 ? 'it' : 'one'
+  end
+
+end
