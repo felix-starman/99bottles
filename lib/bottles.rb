1023624db1827c5633fb4411340b3574081d804d
Peter Mueller <pmueller@covermymeds.com>
2016-08-18 00:08:10 -0400

    refactor into verse modules

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,51 +1,61 @@
-class Line
-  attr_reader :content
-  def initialize
-    @content = content
-  end
-  def to_s
-    content.call
+module Ultimate
+  def self.call(_num)
+    "No more bottles of beer on the wall, no more bottles of beer.\n\
+Go to the store and buy some more, 99 bottles of beer on the wall.\n"
   end
 end
 
-class Verse
-  def initialize(lines)
-    @lines = lines
+module Penultimate
+  def self.call(_num)
+    "1 bottle of beer on the wall, 1 bottle of beer.\n\
+Take it down and pass it around, no more bottles of beer on the wall.\n"
   end
+end
 
-  def to_s
-    lines.map(&:to_s).join
+module Antepenultimate
+  def self.call(_num)
+    "2 bottles of beer on the wall, 2 bottles of beer.\n\
+Take one down and pass it around, 1 bottle of beer on the wall.\n"
+  end
+end
+
+module Default
+  def self.call(num)
+    "#{num} bottles of beer on the wall, #{num} bottles of beer.\n\
+Take one down and pass it around, #{num - 1} bottles of beer on the wall.\n"
   end
 end
 
 class Bottles
+  VERSES = {
+    0 => Ultimate,
+    1 => Penultimate,
+    2 => Antepenultimate
+  }.tap { |h| h.default_proc = proc { |hash, key| hash[key] = Default } }
+
+  attr_reader :song_length
+  def initialize
+    # @verses = {
+    #   0 => Ultimate,
+    #   1 => Penultimate,
+    #   2 => Antepenultimate
+    # }
+    # @verses.default_proc = proc { |hash, key| hash[key] = Default }
+    @song_length = [99, 0]
+  end
+
   def verse(num)
-    # lines.map {|line| self.public_send(line, num) }.join
-    verses[num].map { |verse| verse.call(num) }
+    VERSES[num].call(num)
   end
 
-  def lines
-    [:line1,:line2]
+  def verses(start, finish)
+    start
+      .downto(finish)
+      .each_with_object([]) { |num, acc| acc << VERSES[num].call(num) }
+      .join "\n"
   end
 
-  def line1(num)
-    "#{amount_of_beer_on_wall(num)}, #{amount_of_beer(num).downcase}.\n"
-  end
-
-  def line2(num)
-    "#{action(num)}, #{amount_of_beer_on_wall(num-1).downcase}.\n"
-  end
-
-  def amount_of_beer_on_wall(num)
-    %(#{amount_of_beer(num)} on the wall)
-  end
-
-  def amount_of_beer(num)
-    %(#{num == 0 ? 'No more' : num } #{num == 1 ? 'bottle' : 'bottles' } of beer)
-  end
-
-  def action(num)
-    return "Go to the store and buy some more" if num  == 0
-    "Take #{num == 1 ? 'it' : 'one' } down and pass it around"
+  def song
+    verses(*song_length)
   end
 end
